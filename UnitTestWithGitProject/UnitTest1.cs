248ef3f88d483bd7a1f1eae8ece9ab597805307c
MarkCommitB2 <m20brundieck@gmail.com>
2015-07-25 16:53:05 -0500

    added test, updated test

--- UnitTestWithGitProject/UnitTest1.cs
+++ UnitTestWithGitProject/UnitTest1.cs
@@ -11,7 +11,15 @@ namespace UnitTestWithGitProject
         {
             var string1 = string.Empty;
             var string2 = "test";
-            Assert.AreEqual(string1, string2);
+            Assert.AreEqual(string1, string2, 
+                string.Format("expected {0} and {1} to be the same", string1, string2));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var isBoolTest = true;
+            Assert.IsFalse(isBoolTest,  "Expected False, but failed.");
         }
     }
 }
